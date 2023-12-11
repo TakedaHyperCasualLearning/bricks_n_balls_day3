63f33c9d476823e896ffe112a6730dc95cca2644
TakedaNaomichi <takeda.naomichi@donuts.ne.jp>
2023-12-11 12:19:43 +0900

    CollisionManagerクラスの作成

---
+++ bricks_n_balls_day3/Assets/Scripts/manager/CollisionManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    private Vector2 screenEdge = new Vector2(0, 0);
+
+    public void Initialize()
+    {
+        screenEdge = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+
+    public void Update()
+    {
+    }
+
+    public Vector2 CheckCircleToBox(Vector2 circlePosition, Vector2 boxPosition, float radius, Vector2 size)
+    {
+        Vector2 vector = Vector2.zero;
+
+        if (circlePosition.x + radius < boxPosition.x - size.x) return vector.normalized;
+        if (circlePosition.x - radius > boxPosition.x + size.x) return vector.normalized;
+        if (circlePosition.y + radius < boxPosition.y - size.y) return vector.normalized;
+        if (circlePosition.y - radius > boxPosition.y + size.y) return vector.normalized;
+
+        if (circlePosition.x > boxPosition.x + size.x) vector += Vector2.left;
+        if (circlePosition.x < boxPosition.x - size.x) vector += Vector2.right;
+        if (circlePosition.y > boxPosition.y + size.y) vector += Vector2.down;
+        if (circlePosition.y < boxPosition.y - size.y) vector += Vector2.up;
+
+        return vector.normalized;
+    }
+
+    public Vector2 CheckFieldEdge(Vector2 position, float radius)
+    {
+        Vector2 vector = Vector2.zero;
+
+        if (position.x - radius < -screenEdge.x) vector += Vector2.left;
+        if (position.x + radius > screenEdge.x) vector += Vector2.right;
+        if (position.y - radius < -screenEdge.y) vector += Vector2.down;
+        if (position.y + radius > screenEdge.y) vector += Vector2.up;
+
+        return vector.normalized;
+    }
+}
